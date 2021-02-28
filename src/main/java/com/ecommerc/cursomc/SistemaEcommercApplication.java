8d8ab43e09e11cd6a0eabd3416002770119dad20
Fernandolilo <fernando.sistempro@outlook.com>
2021-02-28 19:21:11 -0300

    Criando Projeto

---
+++ src/main/java/com/ecommerc/cursomc/SistemaEcommercApplication.java
@@ -0,0 +1,18 @@
+package com.ecommerc.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEcommercApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEcommercApplication.class, args);
+	}
+
+}
+
+
+
+
+
---
+++ src/test/java/com/ecommerc/cursomc/SistemaEcommercApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerc.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEcommercApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
