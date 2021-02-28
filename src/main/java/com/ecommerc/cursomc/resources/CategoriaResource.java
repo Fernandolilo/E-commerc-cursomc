43718c9d839ab26344885a6d1fa4f43fedb32ec6
Fernandolilo <fernando.sistempro@outlook.com>
2021-02-28 20:17:11 -0300

    Testando a primeira class de dominio - Categoria

--- src/main/java/com/ecommerc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ecommerc/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.ecommerc.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerc.cursomc.domain.Categoria;
 
 @RestController // Contrele de Rest
 @RequestMapping(value="/categorias") //cria o endepoint
@@ -10,7 +15,16 @@ public class CategoriaResource {
 	
 
 	@RequestMapping(method = RequestMethod.GET) //metodo do request, metodo de busca.
-	public String listar() {
-		return "rest esta funcionando";
+	public List<Categoria> categorias() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		return categorias;
+		
+		
 	}
 }
