957a2f0ec894276d4161b84214d39b21568c2e9a
Fernandolilo <fernando.sistempro@outlook.com>
2021-02-28 21:52:28 -0300

    Criado operação de instaciação

--- src/main/java/com/ecommerc/cursomc/SistemaEcommercApplication.java
+++ src/main/java/com/ecommerc/cursomc/SistemaEcommercApplication.java
@@ -1,13 +1,36 @@
 package com.ecommerc.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ecommerc.cursomc.domain.Categoria;
+import com.ecommerc.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SistemaEcommercApplication {
+public class SistemaEcommercApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaEcommercApplication.class, args);
+		
+	}
+	
+	@Autowired
+	private CategoriaRepository categotiaRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categotiaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
