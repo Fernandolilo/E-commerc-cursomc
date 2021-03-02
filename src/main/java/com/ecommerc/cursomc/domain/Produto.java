673e5822428abc96fd90c441b02d99e0cc69d0ef
Fernandolilo <fernando.sistempro@outlook.com>
2021-03-01 21:20:44 -0300

    Inserindo nova Categoria com POST

--- src/main/java/com/ecommerc/cursomc/domain/Produto.java
+++ src/main/java/com/ecommerc/cursomc/domain/Produto.java
@@ -15,7 +15,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
--- src/main/java/com/ecommerc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/ecommerc/cursomc/resources/CategoriaResource.java
@@ -1,31 +1,40 @@
 package com.ecommerc.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ecommerc.cursomc.domain.Categoria;
 import com.ecommerc.cursomc.service.CategoriaService;
 
-
-
 @RestController // Contrele de Rest
-@RequestMapping(value="/categorias") //cria o endepoint
+@RequestMapping(value = "/categorias") // cria o endepoint
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
 
-	@RequestMapping(value= "/{id}", method = RequestMethod.GET) //metodo do request, metodo de busca.
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET) // metodo do request, metodo de busca.
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		
+
 		Categoria obj = service.find(id);
-		return ResponseEntity.ok().body(obj);	
+		return ResponseEntity.ok().body(obj);
 	}
-	
-	
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		// metodo para disponibilizar a URI do insert no body do Http
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
