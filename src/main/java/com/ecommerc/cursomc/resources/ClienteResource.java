f9b28476523df86f251d444d0cfe9a28d173171e
Fernandolilo <fernando.sistempro@outlook.com>
2021-03-05 20:35:19 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/ecommerc/cursomc/resources/ClienteResource.java
+++ src/main/java/com/ecommerc/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.ecommerc.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,35 +9,48 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ecommerc.cursomc.DTO.ClienteDTO;
+import com.ecommerc.cursomc.DTO.ClienteNewDTO;
+import com.ecommerc.cursomc.DTO.ClienteDTO;
+import com.ecommerc.cursomc.domain.Cliente;
 import com.ecommerc.cursomc.domain.Cliente;
 import com.ecommerc.cursomc.service.ClienteService;
 
-
-
 @RestController // Contrele de Rest
-@RequestMapping(value="/clientes") //cria o endepoint
+@RequestMapping(value = "/clientes") // cria o endepoint
 public class ClienteResource {
-	
+
 	@Autowired
 	private ClienteService service;
 
-	@RequestMapping(value= "/{id}", method = RequestMethod.GET) //metodo do request, metodo de busca.
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET) // metodo do request, metodo de busca.
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
-		
+
 		Cliente obj = service.find(id);
-		return ResponseEntity.ok().body(obj);	
+		return ResponseEntity.ok().body(obj);
 	}
 
+	// insert
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		// metodo para disponibilizar a URI do insert no body do Http
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 
 	// update
+	@Transactional
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
 		Cliente obj = service.fromDTO(objDto);
@@ -45,30 +59,30 @@ public class ClienteResource {
 
 		return ResponseEntity.noContent().build();
 	}
-	@RequestMapping(value = "/{id}", method =RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable Integer id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 
 	// get == buscar por id
 	@RequestMapping(method = RequestMethod.GET) // metodo do request, metodo de busca.
-		public ResponseEntity <List<ClienteDTO>> findAll() {
-			 List<Cliente> list = service.findAll();
-			 List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
-			return ResponseEntity.ok().body(listDTO);
-		}
-	
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
 	// get == buscar por id
-		@RequestMapping(value= "/page",  method = RequestMethod.GET) // metodo do request, metodo de busca.
-			public ResponseEntity <Page<ClienteDTO>> findPage(
-					@RequestParam(value = "page", defaultValue = "0") Integer page, 
-					@RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage, 
-					@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
-					@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
-				 Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
-				 Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
-				return ResponseEntity.ok().body(listDTO);
-			}
-	
+	@RequestMapping(value = "/page", method = RequestMethod.GET) // metodo do request, metodo de busca.
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
+	}
+
 }
--- src/main/java/com/ecommerc/cursomc/service/ClienteService.java
+++ src/main/java/com/ecommerc/cursomc/service/ClienteService.java
@@ -10,55 +10,87 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.ecommerc.cursomc.DTO.ClienteDTO;
+import com.ecommerc.cursomc.DTO.ClienteNewDTO;
+import com.ecommerc.cursomc.domain.Cidade;
 import com.ecommerc.cursomc.domain.Cliente;
+import com.ecommerc.cursomc.domain.Endereco;
+import com.ecommerc.cursomc.domain.enums.TipoCliente;
 import com.ecommerc.cursomc.repositories.ClienteRepository;
+import com.ecommerc.cursomc.repositories.EnderecoRepository;
 import com.ecommerc.cursomc.service.exceptions.ObjectNotFoundException;
 
-@Service //regra de negocio
+@Service // regra de negocio
 public class ClienteService {
-	
-	@Autowired //automanticamente instaciada pelo spring, 
-	//importe da class de tratamento de dados
+
+	@Autowired // automanticamente instaciada pelo spring,
+	// importe da class de tratamento de dados
 	private ClienteRepository repo;
 	
-	
-	public Cliente find(Integer id) {
-		 Optional<Cliente> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
-		}
-	
-	
-		//faz atualização
-		public Cliente update(Cliente obj) {
-			Cliente newObj = find(obj.getId());
-			updateData(newObj, obj);
-			return repo.save(newObj);
-		}
+	@Autowired
+	EnderecoRepository enderecoRepository;
 
-		//busca uma lista de categoria
-		public List<Cliente> findAll() {
-			return repo.findAll();
-		}
-		public void delete(Integer id) {
-			find (id);
-			try {
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	// faz a inserção
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj =repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+
+	// faz atualização
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	// busca uma lista de categoria
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
 			repo.deleteById(id);
-			}catch(DataIntegrityViolationException e) {
-				throw new DataIntegrityViolationException("Não é possível excluir por que há entidade relacionadas");
-			}
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Não é possível excluir por que há entidade relacionadas");
 		}
-		
-		// metodo de paginação para evitar sobrecarga no banco de dados!
-		public Page <Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
-			PageRequest pageResquest = PageRequest.of(page, linesPerPage, org.springframework.data.domain.Sort.Direction.valueOf(direction), orderBy);
-			return repo.findAll(pageResquest);
+	}
+
+	// metodo de paginação para evitar sobrecarga no banco de dados!
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageResquest = PageRequest.of(page, linesPerPage,
+				org.springframework.data.domain.Sort.Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageResquest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()), null, null);
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
 		}
-		
-		public Cliente fromDTO(ClienteDTO objDto) {
-			return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null, null);
+		if(objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
 		}
-		
+		return cli;
+	
+	}
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
