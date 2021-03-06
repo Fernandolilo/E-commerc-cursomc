20745b9a164de6e0e1cf45ccdcf1d04dbd9fd300
Fernandolilo <fernando.sistempro@outlook.com>
2021-03-05 22:45:02 -0300

    validação customisada: EMAIL não repetido na inserção de clientes

--- src/main/java/com/ecommerc/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/ecommerc/cursomc/repositories/ClienteRepository.java
@@ -2,11 +2,13 @@ package com.ecommerc.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ecommerc.cursomc.domain.Cliente;
 
 @Repository //faz o tratamento de acesso a dados
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 	
-
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/ecommerc/cursomc/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/ecommerc/cursomc/service/validation/ClienteInsertValidator.java
@@ -6,16 +6,24 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.ecommerc.cursomc.DTO.ClienteNewDTO;
+import com.ecommerc.cursomc.domain.Cliente;
 import com.ecommerc.cursomc.domain.enums.TipoCliente;
+import com.ecommerc.cursomc.repositories.ClienteRepository;
 import com.ecommerc.cursomc.resources.exceptions.FieldMessage;
 import com.ecommerc.cursomc.service.validation.utils.BR;
 
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
-	@Override
-	public void initialize(ClienteInsert ann) {
-	}
 
+
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	ClienteRepository repo;
+	
+	
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
@@ -26,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURICA.getCod())&& ! BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("Email", "Email já existente"));
+		}
+		
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
