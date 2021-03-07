d6f737878b70d1aaa2f95b0a6ba8fd468776a3e8
Fernandolilo <fernando.sistempro@outlook.com>
2021-03-06 22:56:23 -0300

    SubTotal e Total do pedido

--- src/main/java/com/ecommerc/cursomc/domain/Pedido.java
+++ src/main/java/com/ecommerc/cursomc/domain/Pedido.java
@@ -81,6 +81,14 @@ public class Pedido implements Serializable{
 		this.cliente = cliente;
 		this.endereco = endereco;
 	}
+	
+	public double getValorTotal() {
+		double soma =0;
+		for(ItemPedido ip: itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
 
 	public Integer getId() {
 		return id;
