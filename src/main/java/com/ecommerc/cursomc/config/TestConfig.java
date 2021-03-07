26de0c3caf765a983be68743eb4829fdb52f1438
Fernandolilo <fernando.sistempro@outlook.com>
2021-03-07 20:26:30 -0300

    atualizado Driver do MySql, Procfile, envio de projeto para a produção

--- src/main/java/com/ecommerc/cursomc/config/TestConfig.java
+++ src/main/java/com/ecommerc/cursomc/config/TestConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Profile;
 import com.ecommerc.cursomc.service.DBService;
 
 @Configuration
-@Profile("prod")
+@Profile("test")
 public class TestConfig {
 	
 	@Autowired
